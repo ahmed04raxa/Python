5a33fe7b3432936fb4941448016eac0ff8d2271c
ahmed04raxa <ahmed04raxa@gmail.com>
2025-11-09 12:09:11 +0500

    01_basics

--- 01_basic.py
+++ 01_basic.py
@@ -1,1 +1,62 @@
-print("Hello World!")
+# FIRST PROGRAM
+
+# print("Hello World!")
+
+# VARIABLES
+
+# name = "Ahmed Raza"
+# age = 21
+
+# print(type(name))
+# print(type(age))
+
+# DATE TYPES 
+# => Integers |  +ve, -ve, 0
+
+# age = 21
+# print(age)
+
+# => String | "Ahmed Raza"
+
+# name1 = "AR"  # | MOST PREFERED
+# name2 = 'AR'
+# name3 = '''AR'''
+# print(name1)
+# print(name2)
+# print(name3)
+
+# => Float | "18.9"
+
+# floatNum = 23.5
+# print(floatNum)
+
+# => Boolean | True, False
+
+# old = False
+
+# print(old)
+
+# => None | 
+
+# a = None
+
+# print(type(a))
+
+# TYPES OF OPERATORS
+# 1 | ARTHIMETIC OPERATORS ( +,-, /, %, ** )
+# 2 | RELATIONAL / COMPARISON OPERATORS ( ==, !=, >, <, >=, <= )
+# 3 | ASSIGMENT OPERATORS ( =, +=, -=, /=, %=, **= )
+# 4 | LOGICAL OPERATORS ( not, and, or )
+
+# TYPE CONVERSION
+# a = 2
+# b = 4.25
+
+# sum = a + b
+# print(sum)
+
+# INPUTS IN PYTHON
+
+name = input("Enter Your Name : ")
+
+print("Welcome",name)
