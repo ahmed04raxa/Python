199b4ea97bf04c70ddefdec864544c0b109119d9
ahmed04raxa <ahmed04raxa@gmail.com>
2025-11-24 21:20:10 +0500

    Lecture 06

---
+++ 06_Lecture.py
@@ -0,0 +1,25 @@
+# FUNCTIONS IN PYTHON
+    # Block Of statement that perform a specific task
+
+# BUILT-IN FUNCTION
+    # 1. print()
+    # 1. len()
+    # 1. type()
+    # 1. range()
+
+
+# FUNCTION DEFINATIOM
+# def sum(a,b) : # PARAMETERS
+#     sum = a,b
+#     print(sum)
+
+# sum(4,5) # ARRGUMENTS
+
+# def print_hello() :
+#     print("Hello")
+
+# print_hello()
+
+# RECURSION
+    # When a function calls itself repeatedly.
+    # It Is Remaining To Learn
