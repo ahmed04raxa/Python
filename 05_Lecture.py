16aed51dd1c8bfad75e8e1352533df5adb9ef5b8
ahmed04raxa <ahmed04raxa@gmail.com>
2025-11-24 20:48:30 +0500

    Lecture 05

---
+++ 05_Lecture.py
@@ -0,0 +1,20 @@
+# LOOPS IN PYTHON
+    # Loops are used to repeat instruction
+
+# while Loop
+
+    # while : condition
+        # some work
+
+# i = 5
+
+# while i >=1 :
+#     print(i)
+#     i -= 1
+
+# FOR LOOP
+
+name = ("Ahmed","Raza","Hassan")
+
+for el in name :
+    print(type(el),el)
